f6d6708a1e48e955211803c28996ac3dac8ec8ab
hassaku <hassaku.apps@gmail.com>
2021-01-23 16:59:23 +0900

    Bump version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md") as f:
 
 setup(
     name="audio-plot-lib",
-    version="0.0.4",
+    version="0.0.5",
     description="Plot tools based on audio",
     long_description=long_description,
     long_description_content_type="text/markdown",
